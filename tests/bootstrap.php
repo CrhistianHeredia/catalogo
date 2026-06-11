75b1e12def051c837117eb2d215bc483a2a374dc
Crhistian Heredia Molina <66853258+CrhistianHeredia@users.noreply.github.com>
2026-06-10 23:03:58 -0500

    chore: remove dead CSS vars, unused bootstrap include, old BD.php ref

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 // Load the application files
 require_once __DIR__ . '/../controller/DAO/campo.php';
-require_once __DIR__ . '/../controller/DAO/BD.php';
+require_once __DIR__ . '/../controller/DAO/Connection.php';
 require_once __DIR__ . '/../controller/DAO/usuario.php';
 require_once __DIR__ . '/../controller/DAO/admin.php';
 require_once __DIR__ . '/../controller/auth.php';
